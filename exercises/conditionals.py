748594a19687a5db6d0ba4e8c8c6f69498c70949
aboud20-meet <aboud20@meet.mit.edu>
2018-08-05 16:29:19 +0300

    loops

--- exercises/conditionals.py
+++ exercises/conditionals.py
@@ -1,1 +1,9 @@
-# Write your solution for 1.2 here!
+i=1000
+checking=False
+while checking==False:
+    if i%6==2 and (i**3)%5==3:
+        print(i)
+        checking=True
+    else:
+        i-=1
+        print("Not")
--- exercises/for_loops.py
+++ exercises/for_loops.py
@@ -1,1 +1,6 @@
-# Write your solution for 1.1 here!
+a=0
+i=2
+for i in range(101):
+    if i%2==0:
+        a=a+i
+print(a)
